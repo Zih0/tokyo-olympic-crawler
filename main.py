0a1a212299548b415fdddb6c94628f25bf40cd66
ziho <ziho@kakao.com>
2021-07-28 09:01:21 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
 import json
 from datetime import date
-from github import Github
 from crawl_olympic import parsing_bs, crawl_ranking_data, crawl_medalist_data
 
 
@@ -11,13 +9,16 @@ def gh_update(repo, new_data, file):
     repo.update_file(contents.path, f"update crawling data : {updated}", json.dumps(new_data, ensure_ascii=False), contents.sha, branch="main")
 
 
+def write_json(data, filename):
+    with open(filename,'w') as f:
+        json.dump(data,f , ensure_ascii=False)
+
+
 if __name__ == '__main__':
-    g = Github(os.environ['ACCESS_TOKEN'])
     BASE_URL = 'https://olympics.com/tokyo-2020/olympic-games/ko/results/all-sports/medal-standings.htm'
     soup = parsing_bs(BASE_URL)
     rank_data = crawl_ranking_data(soup)
-    repo = g.get_repo("Zih0/tokyo-olympic-crawler")
-    gh_update(repo, rank_data, "rank_data.json")
     MEDAL_URL = 'https://olympics.com/tokyo-2020/olympic-games/ko/results/all-sports/zzjm094b.json'
     medalist_data = crawl_medalist_data(MEDAL_URL)
-    gh_update(repo, medalist_data, "medalist_data.json")
+    write_json(rank_data,"rank_data.json")
+    write_json(medalist_data, "medalist_data.json")
